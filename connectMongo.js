2599b50a4353380269f9cd12f0477c7688e153b8
Hasan <72514904+devghomo@users.noreply.github.com>
2024-02-10 16:34:57 +0300

    Update connectMongo.js

--- connectMongo.js
+++ connectMongo.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose')
 
 const connectDB = async () => {
     try {
-        await mongoose.connect(process.env.MONGODB_CONNECT_URI)
+        await mongoose.connect("mongodb+srv://devHasan:rEoLSxVloYKnshV7@binayonetim.bwwnfva.mongodb.net/?retryWrites=true&w=majority")
         console.log("Connect to MongoDB successfully")
     } catch (error) {
         console.log("Connect failed " + error.message)
